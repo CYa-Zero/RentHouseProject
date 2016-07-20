59c1ac45b7d5f0037455ebe3d077fb534ca10582
Lonely-Sword <chenyang.on.no@gmail.com>
2016-07-20 15:52:00 -0500

    Initial Project
    This is the start point

---
+++ RentHouseProject/RentHouseProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RentHouseProject
+//
+//  Created by Chenyang on 7/20/16.
+//  Copyright © 2016 Chenyang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ RentHouseProject/RentHouseProject/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  RentHouseProject
+//
+//  Created by Chenyang on 7/20/16.
+//  Copyright © 2016 Chenyang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
